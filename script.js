d282a1c3fe5704a591fb54d80f110ab4bc332601
Felipe do Prado <131405659+felipe-durval@users.noreply.github.com>
2024-01-30 14:12:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function mostrarMais(){
     }
 
 }
+
+let btnMenu = document.getElementById("btn-abrir-menu")
+let menu = document.getElementById("menu-mobile")
+let overlay =document.getElementById("overlay-menu")
+
+btnMenu.addEventListener('click' , () =>{
+    menu.classList.add('abrirMenu')
+})
+
+menu.addEventListener('click' , () =>{
+    menu.classList.remove("abrirMenu")
+})
+
+overlay.addEventListener('click' , () =>{
+    menu.classList.remove("abrirMenu")
+})
